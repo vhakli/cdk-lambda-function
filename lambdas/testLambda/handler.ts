5bd1a72c8854500811b19862257e0c41c0db0d09
Ville Häkli <ville.hakli@vaisala.com>
2023-03-08 16:54:24 +0200

    Example stuff

--- lambdas/testLambda/handler.ts
+++ lambdas/testLambda/handler.ts
@@ -1,5 +1,7 @@
 import { Axios } from "@villehx/axios";
+import { captureAWS } from "@villehx/xray";
 
 export const handler = async (event: any) => {
+  console.log("captureAWS", captureAWS);
   console.log("AXIOS", Axios);
 };
--- lib/stacks/lambda-stack.ts
+++ lib/stacks/lambda-stack.ts
@@ -2,10 +2,20 @@ import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const axiosLayerArn = StringParameter.valueForStringParameter(
+      this,
+      "axios-layer-arn"
+    );
+    const xrayLayerArn = StringParameter.valueForStringParameter(
+      this,
+      "xray-layer-arn"
+    );
 
     new NodejsFunction(this, "testLambda", {
       entry: "./lambdas/testLambda/handler.ts",
@@ -14,14 +24,11 @@ export class LambdaStack extends Stack {
       timeout: Duration.seconds(5),
       bundling: {
         sourceMap: true,
-        externalModules: ["aws-sdk", "@villehx/axios"],
+        externalModules: ["aws-sdk", "@villehx/axios", "@villehx/xray"],
       },
       layers: [
-        LayerVersion.fromLayerVersionArn(
-          this,
-          "axiosLayer",
-          "arn:aws:lambda:us-east-1:522496507093:layer:axioslayer29F42384:9"
-        ),
+        LayerVersion.fromLayerVersionArn(this, "axiosLayer", axiosLayerArn),
+        LayerVersion.fromLayerVersionArn(this, "xrayLayer", xrayLayerArn),
       ],
     });
   }
